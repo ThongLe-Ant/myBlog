89529516b789f9034740d42a9cf1a3b4e617e262
Leminh Thongpro <Thongproleminh@gmail.com>
2025-08-20 14:45:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,12 +31,12 @@ export default function RootLayout({
         >
           <LanguageProvider>
             <div className="flex flex-col min-h-screen">
-              <Header />
-              <main className="flex-grow">{children}</main>
-              <AppFooter />
+                <Header />
+                <main className="flex-grow">{children}</main>
+                <AppFooter />
+                <Toaster />
+                <GoToTopButton />
             </div>
-            <Toaster />
-            <GoToTopButton />
           </LanguageProvider>
         </ThemeProvider>
       </body>
